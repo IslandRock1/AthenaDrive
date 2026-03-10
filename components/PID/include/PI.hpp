ed6171439c5d851b13c762f1ed41e7ae25e52fd7
KineVictori <kine.aurora@hotmail.com>
2026-03-10 11:01:50 +0100

    added PI

---
+++ components/PID/include/PI.hpp
@@ -0,0 +1,13 @@
+#pragma once
+// Using a separate PI controller will
+// make the inner PI torque loop go faster.
+
+class PI {
+public:
+    PI(float kp, float ki);
+    float update(float error);
+
+private:
+    float _kp, _ki;
+    float _integral = 0.0;
+};
