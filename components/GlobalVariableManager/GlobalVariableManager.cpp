c6689c3a672ec39bee79cb251fc509ee2f1ed001
Kine <143505485+KineVictori@users.noreply.github.com>
2026-04-30 13:47:34 +0200

    Global var dev
    * opprette filer
    
    * Rename GVM
    
    * Alle glob lagt til, ikke ryddet main eller switch case
    
    * added acc
    
    * added acc
    
    * fjerna acc: updateFreq og setpoint
    
    * Add getters and setters for update frequency transmission
    
    * deleted switch case
    
    * cleanup
    
    * cleanup

---
+++ components/GlobalVariableManager/GlobalVariableManager.cpp
@@ -0,0 +1,177 @@
+#include "GlobalVariableManager.hpp"
+
+#include <cstring>
+
+
+GlobalVariableManager::GlobalVariableManager() = default;
+
+void GlobalVariableManager::atomic_store_float(std::atomic_uint32_t& atomicValue, float value) {
+    static_assert(sizeof(float) == sizeof(uint32_t), "float must be 32-bit");
+    uint32_t bits = 0;
+    std::memcpy(&bits, &value, sizeof(bits));
+    atomicValue.store(bits, std::memory_order_relaxed);
+}
+
+float GlobalVariableManager::atomic_load_float(std::atomic_uint32_t& atomicValue) {
+    static_assert(sizeof(float) == sizeof(uint32_t), "float must be 32-bit");
+    uint32_t bits = atomicValue.load(std::memory_order_relaxed);
+    float value = 0.0f;
+    std::memcpy(&value, &bits, sizeof(value));
+    return value;
+}
+
+// Measurements / states
+int32_t GlobalVariableManager::getRotations() {
+    return _rotations.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setRotations(int32_t value) {
+    _rotations.store(static_cast<uint32_t>(value), std::memory_order_relaxed);
+}
+
+float GlobalVariableManager::getAngle() {
+    return atomic_load_float(_angle);
+}
+
+void GlobalVariableManager::setAngle(float value) {
+    atomic_store_float(_angle, value);
+}
+
+float GlobalVariableManager::getCumAngle() {
+    return atomic_load_float(_cum_angle);
+}
+
+void GlobalVariableManager::setCumAngle(float value) {
+    atomic_store_float(_cum_angle, value);
+}
+
+float GlobalVariableManager::getVelocity() {
+    return atomic_load_float(_velocity);
+}
+
+void GlobalVariableManager::setVelocity(float value) {
+    atomic_store_float(_velocity, value);
+}
+
+float GlobalVariableManager::getAvgVelocity() {
+    return atomic_load_float(_avg_velocity);
+}
+
+void GlobalVariableManager::setAvgVelocity(float value) {
+    atomic_store_float(_avg_velocity, value);
+}
+
+float GlobalVariableManager::getAcceleration() {
+    return atomic_load_float(_acceleration);
+}
+
+void GlobalVariableManager::setAcceleration(float value) {
+    atomic_store_float(_acceleration, value);
+}
+
+float GlobalVariableManager::getAvgAcceleration() {
+    return atomic_load_float(_avg_acceleration);
+}
+
+void GlobalVariableManager::setAvgAcceleration(float value) {
+    atomic_store_float(_avg_acceleration, value);
+}
+
+float GlobalVariableManager::getAvgStrength() {
+    return atomic_load_float(_avg_strength);
+}
+
+void GlobalVariableManager::setAvgStrength(float value) {
+    atomic_store_float(_avg_strength, value);
+}
+
+float GlobalVariableManager::getAvgLoopTime() {
+    return atomic_load_float(_avg_loop_time);
+}
+
+void GlobalVariableManager::setAvgLooptime(float value) {
+    atomic_store_float(_avg_loop_time, value);
+}
+
+uint32_t GlobalVariableManager::getVoltage() {
+    return _voltage.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setVoltage(uint32_t value) {
+    _voltage.store(value, std::memory_order_relaxed)
+}
+
+uint32_t GlobalVariableManager::getDrivingMode() {
+    return _driving_mode.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setDrivingMode(uint32_t value) {
+    _driving_mode.store(value, std::memory_order_relaxed);
+}
+
+int32_t GlobalVariableManager::getCurrentLimit() {
+    return static_cast<int32_t>(_current_limit.load(std::memory_order_relaxed));
+}
+
+void GlobalVariableManager::setCurrentLimit(int32_t value) {
+    _current_limit.store(static_cast<uint32_t>(value), std::memory_order_relaxed);
+}
+
+uint32_t GlobalVariableManager::getUpdateFreqTransmition() {
+    return _update_freq_transmition.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setUpdateFreqTransmition(uint32_t value) {
+    _update_freq_transmition.store(value, std::memory_order_relaxed);
+}
+
+// Torque
+float GlobalVariableManager::getTorqueSetpoint() {
+    return atomic_load_float(_torque_setpoint);
+}
+
+void GlobalVariableManager::setTorqueSetpoint(float value) {
+    atomic_store_float(_torque_setpoint, value);
+}
+
+uint32_t GlobalVariableManager::getUpdateFreqTorque() {
+    return _update_freq_torque.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setUpdateFreqTorque(uint32_t value) {
+    _update_freq_torque.store(value, std::memory_order_relaxed);
+}
+
+// Velocity
+float GlobalVariableManager::getVelocitySetpoint() {
+    return atomic_load_float(_velocity_setpoint);
+}
+
+void GlobalVariableManager::setVelocitySetpoint(float value) {
+    atomic_store_float(_velocity_setpoint, value);
+}
+
+uint32_t GlobalVariableManager::getUpdateFreqVelocity() {
+    return _update_freq_velocity.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setUpdateFreqVelocity(uint32_t value) {
+    _update_freq_velocity.store(value, std::memory_order_relaxed);
+}
+
+// Position
+float GlobalVariableManager::getPositionSetpoint() {
+    return atomic_load_float(_position_setpoint);
+}
+
+void GlobalVariableManager::setPositionSetpoint(float value) {
+    atomic_store_float(_position_setpoint, value);
+}
+
+uint32_t GlobalVariableManager::getUpdateFreqPosition() {
+    return _update_freq_position.load(std::memory_order_relaxed);
+}
+
+void GlobalVariableManager::setUpdateFreqPosition(uint32_t value) {
+    _update_freq_position.store(value, std::memory_order_relaxed);
+}
---
+++ components/GlobalVariableManager/include/GlobalVariableManager.hpp
@@ -0,0 +1,104 @@
+
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+
+
+class GlobalVariableManager {
+public:
+    GlobalVariableManager();
+
+    // Measurements / state
+    int32_t getRotations();
+    void setRotations(int32_t value);
+
+    float getAngle();
+    void setAngle(float value);
+
+    float getCumAngle();
+    void setCumAngle(float value);
+
+    float getVelocity();
+    void setVelocity(float value);
+
+    float getAvgVelocity();
+    void setAvgVelocity(float value);
+
+    float getAcceleration();
+    void setAcceleration(float value);
+
+    float getAvgAcceleration();
+    void setAvgAcceleration(float value);
+
+    float getAvgStrength();
+    void setAvgStrength(float value);
+
+    float getAvgLoopTime();
+    void setAvgLooptime(float value);
+
+    uint32_t getVoltage();
+    void setVoltage(uint32_t value);
+
+    uint32_t getDrivingMode();
+    void setDrivingMode(uint32_t value);
+
+    int32_t getCurrentLimit();
+    void setCurrentLimit(int32_t value);
+
+    uint32_t getUpdateFreqTransmition();
+    void setUpdateFreqTransmition(uint32_t value);
+
+    // Torque
+    float getTorqueSetpoint();
+    void setTorqueSetpoint(float value);
+
+    uint32_t getUpdateFreqTorque();
+    void setUpdateFreqTorque(uint32_t value);
+
+    // Velocity
+    float getVelocitySetpoint();
+    void setVelocitySetpoint(float value);
+
+    uint32_t getUpdateFreqVelocity();
+    void setUpdateFreqVelocity(uint32_t value);
+
+    // Position
+    float getPositionSetpoint();
+    void setPositionSetpoint(float value);
+
+    uint32_t getUpdateFreqPosition();
+    void setUpdateFreqPosition(uint32_t value);
+
+
+private:
+    void atomic_store_float(std::atomic_uint32_t& atomicValue, float value);
+    float atomic_load_float(std::atomic_uint32_t& atomicValue);
+
+    std::atomic_uint32_t _rotations{0};
+
+    std::atomic_uint32_t _angle{0};
+    std::atomic_uint32_t _cum_angle{0};
+    std::atomic_uint32_t _velocity{0};
+    std::atomic_uint32_t _acceleration{0};
+
+    std::atomic_uint32_t _avg_velocity{0};
+    std::atomic_uint32_t _avg_acceleration{0};
+    std::atomic_uint32_t _avg_strength{0};
+    std::atomic_uint32_t _avg_loop_time{0};
+
+    std::atomic_uint32_t _voltage{0};
+    std::atomic_uint32_t _driving_mode{0};
+    std::atomic_uint32_t _current_limit{0};
+
+    std::atomic_uint32_t _update_freq_transmition{100};     // ms
+
+    std::atomic_uint32_t _torque_setpoint{0};
+    std::atomic_uint32_t _update_freq_torque{1};
+
+    std::atomic_uint32_t _velocity_setpoint{0};
+    std::atomic_uint32_t _update_freq_velocity{10};
+
+    std::atomic_uint32_t _position_setpoint{0};
+    std::atomic_uint32_t _update_freq_position{100};
+};
