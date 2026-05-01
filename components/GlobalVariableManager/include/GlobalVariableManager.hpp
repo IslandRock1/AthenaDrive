9307f6f1945e38b16a5d76e523dde9732e1800e2
Øystein Bringsli <oysteinmb@gmail.com>
2026-05-01 12:40:08 +0200

    Dynamic pid changing

--- components/GlobalVariableManager/include/GlobalVariableManager.hpp
+++ components/GlobalVariableManager/include/GlobalVariableManager.hpp
@@ -10,6 +10,12 @@ public:
     GlobalVariableManager();
 
     // Measurements / state
+    bool getWantedCalibrationMode();
+    void setWantedCalibrationMode(bool value);
+
+    bool getActualCalibrationMode();
+    void setActualCalibrationMode(bool value);
+
     int32_t getRotations();
     void setRotations(int32_t value);
 
@@ -50,6 +56,12 @@ public:
     void setUpdateFreqTransmition(uint32_t value);
 
     // Torque
+    float getTorqueKp();
+    void setTorqueKp(float value);
+
+    float getTorqueKi();
+    void setTorqueKi(float value);
+
     float getTorqueSetpoint();
     void setTorqueSetpoint(float value);
 
@@ -57,6 +69,15 @@ public:
     void setUpdateFreqTorque(uint32_t value);
 
     // Velocity
+    float getVelocityKp();
+    void setVelocityKp(float value);
+
+    float getVelocityKi();
+    void setVelocityKi(float value);
+
+    float getVelocityKd();
+    void setVelocityKd(float value);
+
     float getVelocitySetpoint();
     void setVelocitySetpoint(float value);
 
@@ -64,6 +85,15 @@ public:
     void setUpdateFreqVelocity(uint32_t value);
 
     // Position
+    float getPositionKp();
+    void setPositionKp(float value);
+
+    float getPositionKi();
+    void setPositionKi(float value);
+
+    float getPositionKd();
+    void setPositionKd(float value);
+
     float getPositionSetpoint();
     void setPositionSetpoint(float value);
 
@@ -75,6 +105,8 @@ private:
     void atomic_store_float(std::atomic_uint32_t& atomicValue, float value);
     float atomic_load_float(std::atomic_uint32_t& atomicValue);
 
+    std::atomic_bool _wantedCalibrationMode{false};
+    std::atomic_bool _actualCalibrationMode{false};
     std::atomic_uint32_t _rotations{0};
 
     std::atomic_uint32_t _angle{0};
@@ -93,12 +125,20 @@ private:
 
     std::atomic_uint32_t _update_freq_transmition{100};     // ms
 
+    std::atomic_uint32_t _torqueKp;
+    std::atomic_uint32_t _torqueKi;
     std::atomic_uint32_t _torque_setpoint{0};
     std::atomic_uint32_t _update_freq_torque{1};
 
+    std::atomic_uint32_t _velocityKp;
+    std::atomic_uint32_t _velocityKi;
+    std::atomic_uint32_t _velocityKd;
     std::atomic_uint32_t _velocity_setpoint{0};
     std::atomic_uint32_t _update_freq_velocity{10};
 
+    std::atomic_uint32_t _positionKp;
+    std::atomic_uint32_t _positionKi;
+    std::atomic_uint32_t _positionKd;
     std::atomic_uint32_t _position_setpoint{0};
     std::atomic_uint32_t _update_freq_position{100};
 };
